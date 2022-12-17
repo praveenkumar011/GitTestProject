645ffd5f898f1b90ee427be8d975c682cd8b1b49
praveenkumar011 <30856996+praveenkumar011@users.noreply.github.com>
2022-12-17 22:26:45 +0000

    if condition added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public void addData(String data)
     {
         System.out.println("Add data -->"+ data);
+        if(data.equals("Java Techie"))
+            System.out.println("Welcome");
     }
     public static void main(String[] args) {
 
