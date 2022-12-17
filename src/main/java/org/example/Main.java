07853182b6f10a532b95d6c9dcef78700669a0e1
praveenkumar011 <30856996+praveenkumar011@users.noreply.github.com>
2022-12-17 22:20:20 +0000

    Git Demo Project initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+
+    public void addData(String data)
+    {
+        System.out.println("Add data -->"+ data);
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
